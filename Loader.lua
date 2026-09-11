f68ff720d914c45cc5e65fedf3815cf0ff18747b
khoalang612 <kkk261116@gmail.com>
2026-09-11 16:50:05 +0700

    Add loadstring loader script

---
+++ Loader.lua
@@ -0,0 +1,4 @@
+-- Roblox Invisible Button GUI - Loadstring Version
+-- Copy toàn bộ đoạn code này và dán vào Command Bar hoặc Script
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/khoalang612/roblox-invisible-button/main/InvisibleButton.lua"))()
